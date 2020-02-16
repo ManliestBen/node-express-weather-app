5e10783d5efd6209b34a010150dfebff1bc6bbbf
ManliestBen <benjamintmanley@gmail.com>
2020-02-15 20:17:36 -0600

    rework post request to include api call

--- server.js
+++ server.js
@@ -11,27 +11,28 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 
 let apiKey = process.env.APIKEY;
-let city = 'austin';
-let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
 
 
 
-request(apiUrl, function (err, response, body) {
-    if(err){
-      console.log('error:', error);
-    } else {
-        let weather = JSON.parse(body);
-        let message = `It's ${weather.main.temp} degrees in ${weather.name}!`;
-        console.log(message);    
-    }
-    }
-);
 
 app.get('/', function (req, res) {
   res.render('index');
 });
 
 app.post('/', function (req, res) {
+  let city = req.body.city;
+  let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
+  request(apiUrl, function (err, response, body) {
+      if(err){
+        res.render('index', {weather: null, error: 'Please try again'});
+      } else {
+          let weather = JSON.parse(body);
+          console.log(weather)
+          let message = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+            
+      }
+      }
+  );
   res.render('index');
   console.log(req.body.city);
 })
